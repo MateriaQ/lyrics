89e8bbb6d571800d859f1553914915f09ad83c6c
Sanooj Es <mail@sanooj.uk>
2026-09-18 20:54:45 +0530

    fix: / redirect

--- apps/backend/src/routes/index.route.ts
+++ apps/backend/src/routes/index.route.ts
@@ -2,7 +2,7 @@ import { t } from "elysia";
 import { createRouter } from "@/utils/app";
 
 export const indexRouter = createRouter({ name: "root" })
-  .get("/", ({ redirect }) => redirect("/docs", "302"), {
+  .get("/", ({ redirect }) => redirect("/api/docs", "302"), {
     detail: {
       summary: "Root Check",
       tags: ["System"],
